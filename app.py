c9426375fdb656f4746c35243b7b8a9f2a771a13
patrickpulfer <Patrick.Pulfer1@gmail.com>
2021-05-02 00:34:52 +0100

    Feature: Add Logging into file
    Added formatted logging feature to the app which will be saved to app.log

--- app.py
+++ app.py
@@ -3,6 +3,17 @@ import os
 if os.path.exists("env.py"):
     import env
 import pymongo
+import logging
+
+# Setting up Logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="{asctime} {levelname:<8} {message}",
+    style='{'
+    # filename='logs.log
+    # filemode='a'
+)
+logging.info('app.py initalized!')
 
 
 app = Flask(__name__)
@@ -10,21 +21,30 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 # MongoDB Setup
 MONGO_URI = os.environ.get("MONGO_URI")
-DATABASE = "milestone2"
+DATABASE = "TheInterviewMasterDeck"
 COLLECTION = "Admin"
 
 
 def mongo_connect(url):
     try:
         conn = pymongo.MongoClient(url)
-        print("Mongo is connected")
+        logging.info('MongoDB Connected!')
         return conn
     except pymongo.errors.ConnectionFailure as e:
+        logging.critical('Could not connect to MongoDB: %s', e)
         print("Could not connect to MongoDB: %s") % e
 
 
 conn = mongo_connect(MONGO_URI)
 coll = conn[DATABASE][COLLECTION]
+logging.info('MongoDB Server version: %s', conn.server_info()["version"])
+
+
+dblist = conn.list_database_names()
+if DATABASE in dblist:
+    print("The database exists.")
+else:
+    print('no')
 
 
 @app.route("/")
