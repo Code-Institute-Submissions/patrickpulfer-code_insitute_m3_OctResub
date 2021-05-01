536eca33df3196f7a2b00fe384d53201a8842858
patrickpulfer <Patrick.Pulfer1@gmail.com>
2021-05-01 17:46:23 +0100

    Adding Pymongo & testing connection to mongodb.com

--- app.py
+++ app.py
@@ -2,23 +2,34 @@ from flask import Flask, render_template, request, flash
 import os
 if os.path.exists("env.py"):
     import env
+import pymongo
 
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
 
+# MongoDB Setup
+MONGO_URI = os.environ.get("MONGO_URI")
+DATABASE = "milestone2"
+COLLECTION = "Admin"
+
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+
+conn = mongo_connect(MONGO_URI)
+coll = conn[DATABASE][COLLECTION]
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
-
-
-@app.route("/contact", methods=["GET", "POST"])
-def contact():
-    if request.method == "POST":
-        flash("Thanks {}, we have received your message!".format(
-            request.form.get("name")))
-    return render_template("contact.html", page_title="Contact")
 
 
 if __name__ == "__main__":
