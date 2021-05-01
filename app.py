a7c032cbe82f77acb64a4092948d78810528c0ad
patrickpulfer <Patrick.Pulfer1@gmail.com>
2021-05-01 16:58:42 +0100

    Setting up folder structures

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, flash
+import os
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
+    return render_template("contact.html", page_title="Contact")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
